94142a1c0a67618317a0be49013d6bb6e55836a2
Francesco Minna <fra1995m@gmail.com>
2023-07-10 12:41:18 +0200

    removed print unique checks

--- .github/scripts/kubelinter_fix_chart.py
+++ .github/scripts/kubelinter_fix_chart.py
@@ -48,7 +48,6 @@ def iterate_checks(chart_folder: str, json_path: str) -> None:
 
     # Print all found checks
     all_checks = [x for x in all_checks if x is not None]
-    all_checks = list(dict.fromkeys(all_checks))
     all_checks.sort()
     print(f"Total number of checks: {len(all_checks)}")
     print(", ".join(all_checks))
